41b55302bfe3561214fe731cb4f5700ac4a82dea
SudoShea <sheaclark95@outlook.com>
2026-07-28 23:18:13 +0930

    fix(scripts)/refactor(hardening): bump version to v1.0.1

--- files/restic-restore.sh
+++ files/restic-restore.sh
@@ -3,7 +3,7 @@
 # File        : files/restic-restore.sh
 # Description : Interactive CLI helper utility for restoring and mounting Restic snapshots
 # Author      : SudoShea
-# Version     : 1.0.0
+# Version     : 1.0.1
 # License     : MIT
 # ==============================================================================
 
